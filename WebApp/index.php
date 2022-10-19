ea6397d7a4cb1575a5b4541adf29204ae33079bc
Niruthub <s6303051623055@email.kmutnb.ac.th>
2022-10-19 11:58:55 +0700

    save_bootstrap_V3

--- WebApp/index.php
+++ WebApp/index.php
@@ -11,7 +11,12 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=css_all.css>
+    <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!--Icon-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <title>Web App_Nirut_mini</title>
 </head>
 <?php 
@@ -19,40 +24,59 @@ session_start();
     if(!isset($_SESSION['id'])){
     ?>
 <body>
+    <div class="container">
     <h1 style="text-align: center;">Web App_Nirut_mini</h1>
-    <hr>
-    หมวดหมู่: <select name="learn">
-        <option value="--All--">--ทั้งหมด--</option>
-        <option value="normal">เรื่องทั่วไป</option>
-        <option value="learn">เรื่องเรียน</option>
-    </select>
-
-    <a href="login.php"style="float: right;" >เข้าสู่ระบบ</a>
+    <?php include "nav.php"; ?>
     <br>
-    <ul>
+    <div class="d-flex">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+            <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">
+                --ทั้งหมด--
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="buttton2">
+                <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+            </ul>
+            </span>
+        </div>
+    </div>
+    <br>
+    <table class="table table-striped">
+    
         <?php 
             //$n = 1;
             for($i=1;$i<=10;$i++)
-            echo "<li><a href='post.php?id=$i'>กระทู้ที่ $i</a></li>";
+            echo "<tr><td><a href='post.php?id=$i' style='text-decoration:none;'>กระทู้ที่ $i</a></td></tr>";
 
-        ?>
-        
-    </ul>
+        ?>  
+    
+    </table>
+    </div>
 </body>
 <?php }else{ ?>
     <body>
+    <div class="container">
     <h1 style="text-align: center;">Web App_Nirut_mini</h1>
-    <hr>
-    หมวดหมู่: <select name="learn">
-        <option value="--All--">--ทั้งหมด--</option>
-        <option value="normal">เรื่องทั่วไป</option>
-        <option value="learn">เรื่องเรียน</option>
-    </select>
-    <div style="float: right;">
-        <p>ผู้ใช้งานระบบ : <?php echo $_SESSION['username']." "?>&nbsp;&nbsp;
-        <a href="logout.php"> ออกจากระบบ</a></p>
-    </div>
+    <?php include "nav.php"; ?>
     <br>
+    <div class="d-flex">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+            <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">
+                --ทั้งหมด--
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="buttton2">
+                <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+            </ul>
+            </span>
+        </div>
+    </div>
     <a href="newpost.php">สร้างกระทู้ใหม่</a>
     <br>
     <ul>
@@ -66,6 +90,7 @@ session_start();
         ?>
         
     </ul>
+    </div>
 </body>
     <?php } ?> 
       
